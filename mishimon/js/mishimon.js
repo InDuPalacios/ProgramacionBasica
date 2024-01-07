c3aa215a17bcae52600bed676aa088402f7339b7
Indu <indira.duranp@gmail.com>
2024-01-07 20:31:30 +0100

    Ataques aleatorios

--- mishimon/js/mishimon.js
+++ mishimon/js/mishimon.js
@@ -1,6 +1,18 @@
+let ataqueJugador 
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
 }
 
 function seleccionarMascotaJugador(){
@@ -25,6 +37,7 @@ function seleccionarMascotaJugador(){
    seleccionarMascotaEnemigo()
 }
 
+
 function seleccionarMascotaEnemigo(){
     let mascotaEnemigo = aleatorio (1,3)
     let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
@@ -44,5 +57,34 @@ function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function ataqueTierra(){
+    ataqueJugador = "TIERRA"
+
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueFuego(){
+    ataqueJugador = "FUEGO"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua(){
+    ataqueJugador = "AGUA"
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAletorio = aleatorio(1,3)
+    
+    if (ataqueAletorio == 3){
+        ataqueEnemigo = "agua"
+    }
+    else if (ataqueAletorio == 2){
+        ataqueEnemigo = "tierra"
+    }
+    else{
+        ataqueEnemigo = "fuego"
+    }
+}
 
 window.addEventListener("load", iniciarJuego)
