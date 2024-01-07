a5670275d656a7c907cd5459a2c29f5521bc4690
Indu <indira.duranp@gmail.com>
2024-01-07 19:05:38 +0100

    addEventListener de JavaScript

--- mishimon/js/mishimon.js
+++ mishimon/js/mishimon.js
@@ -4,26 +4,45 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador(){
-    let inputHipodogue = document.getElementById("hipodoge")
-    let inputCapipepo = document.getElementById("capipepo")
-    let inputRatigueya = document.getElementById("ratigueya")
+    let inputGrowmon = document.getElementById("growmon")
+    let inputFiremon = document.getElementById("firemon")
+    let inputWaltermon = document.getElementById("waltermon")
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
 
-   if (inputHipodogue.checked){
-    alert("Seleccionaste HIPODOGE")
+   if (inputGrowmon.checked){
+    spanMascotaJugador.innerHTML = "Growmon"
    }
-   else if (inputCapipepo.checked){
-    alert("Seleccionaste CAPIPEPO")
+   else if (inputFiremon.checked){
+    spanMascotaJugador.innerHTML = "Firemon"
    }
-   else if (inputRatigueya.checked){
-    alert("Seleccionaste RATIGUEYA")
+   else if (inputWaltermon.checked){
+    spanMascotaJugador.innerHTML = "Waltermon"
    }
    else {
     alert ("no seleccionaste tu mascota")
-   }
- 
-    
-       
+   } 
+
+   seleccionarMascotaEnemigo()
 }
-   
+
+function seleccionarMascotaEnemigo(){
+    let mascotaEnemigo = aleatorio (1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+    
+    if (mascotaEnemigo == 1){
+        spanMascotaEnemigo.innerHTML = "DEARTHMON"
+       }
+       else if (mascotaEnemigo == 2){
+        spanMascotaEnemigo.innerHTML = "DEFIREMON"
+       }
+       else {
+        spanMascotaEnemigo.innerHTML = "DEWATERMON"
+       }
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 
 window.addEventListener("load", iniciarJuego)
