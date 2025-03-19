eb24b1769af1aa093ec9aace0b19746fd0b85faf
Indu <indira.duranp@gmail.com>
2025-03-19 11:04:28 +0100

    Add numerical operators examples with assignment and comparison

---
+++ Python/math_ops.py
@@ -0,0 +1,31 @@
+# Numerical Operators
+
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Substraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("Moduls:", a % b)
+print("Quotient:", a // b)
+# Double division (//) returns the integer part of the division
+print("Exponent:", a ** b)
+
+# Use of assignment operators
+a += 2
+print(a)
+
+# PEMDAS (Order of operations in mathematics)
+# Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+# Comparison operators
+print(a > b)
+print(a < b)
+
+print(a >= b)
+print(a <= b)
+
+print(a == b)
+print(a != b)
