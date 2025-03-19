72e12abb789cd42c4f8b36ef622a1568932e5196
Indu <indira.duranp@gmail.com>
2025-03-19 10:39:24 +0100

    Firts Steps in Python

---
+++ Python/begin.py
@@ -0,0 +1,26 @@
+print ("Hola Mundo!!")
+
+print("Nunca", "pares", "de", "aprender")
+
+print("Nunca" + "pares" + "de" + "aprender")
+
+print("Nunca", "pares", "de", "aprender", sep=", ")
+
+print("Nunca", end=" ")
+print("pares de aprender")
+
+frase = "Nunca pares de aprender"
+author = "Platzi"
+print("Frase:", frase, "Autor:", author)
+
+print(f"Frase: {frase}, Autor: {author}")
+
+print("Frase: {}, Autor: {}".format(frase, author))
+
+valor = 3.14159
+print("Valor: {:.2f}".format(valor))
+
+print("Hola\nmundo")
+
+# Metodos en Py : funciones asociadas a objetos para manipularlos 
+# Sintaxis de Py
